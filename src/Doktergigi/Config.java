c3c305b5df1ee1da460f0beebca0bb62139c1ba4
radityanatha <radityanatha20@gmail.com>
2024-11-24 13:43:16 +0800

    Upload Project

---
+++ src/Doktergigi/Config.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Doktergigi;
+
+/**
+ *
+ * @author radit
+ */
+public class Config {
+    public static void main(String[] args) {
+        
+    }
+}
